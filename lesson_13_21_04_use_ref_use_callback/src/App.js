23e80fba95190c74a55fa40a38046064303a6d38
AlexDolz <aleksejdolznikov3@gmail.com>
2023-04-24 17:20:40 +0200

    updated with comments

--- lesson_13_21_04_use_ref_use_callback/src/App.js
+++ lesson_13_21_04_use_ref_use_callback/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 
 const App = () => {
   let ulRef = useRef();
+  // Пример useRef, который содержит не только target тега, но также может хранить число, строку, массив и тд
   let numberRef = useRef();
 
   let [number, setNumber] = useState([1, 2, 3, 4, 5]);
 
+  // Обновление numberRef
   numberRef.current = number;
 
   // мини задание: напишите функцию, которая будет добавлять в массив новый элемент
   // занчение которого будет - последний элемент + 1
+
+  // Задача useRef - создание объекта, значение которого после обновления не будет переопределяться
+  // Дополнительно хранит всегда актуальные данные (даже если мы его используем в useCallback)
+  // useRef далее заменяет querySelector и тд внутри react приложения
 
   const addNumber = () => {
     setNumber([...number, number.length + 1]);
@@ -20,6 +26,10 @@ const App = () => {
     ulRef.current.removeEventListener('scroll', handler);
   };
 
+  // useCallback - это хук, который позволяет функции не переопределяться после
+  // обновления компонента.
+  // Хук позволяет указать зависимости (режим работы, когда нужно или не нужно перопределять функцию)
+  // так же, как это делает useEffect
   const handler = useCallback(
     () => console.log('scroll', number, numberRef.current),
     []
@@ -31,6 +41,7 @@ const App = () => {
 
   return (
     <div>
+      {/* Указание рефа внутри тега используя атрибут ref */}
       <ul ref={ulRef}>
         {number.map((elem, index) => (
           <li key={index}>{elem}</li>
