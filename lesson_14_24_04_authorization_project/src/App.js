05883466293ef0ec3511030ee0d23f2b2e2953c5
AlexDolz <aleksejdolznikov3@gmail.com>
2023-04-26 12:43:37 +0200

    updated

--- lesson_14_24_04_authorization_project/src/App.js
+++ lesson_14_24_04_authorization_project/src/App.js
@@ -4,8 +4,6 @@ import Button from './components/UI/Button/Button';
 import { Link } from 'react-router-dom';
 import Modal from './components/Modal/Modal';
 
-// Реализовать в компоненте Modal кнопку 'X', который закроет моадльное окно
-
 const App = () => {
   const [active, setActive] = useState(false);
 
@@ -14,9 +12,9 @@ const App = () => {
       <Link to={'/reg'}>
         <Button
           onClick={() => setActive(true)}
-          title={'Authorization'}
+          title={'Registration / Login'}
           color={'yellow'}
-        />{' '}
+        />
       </Link>
       <Modal setActive={setActive} active={active} />
     </div>
--- lesson_14_24_04_authorization_project/src/components/Modal/Modal.js
+++ lesson_14_24_04_authorization_project/src/components/Modal/Modal.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import s from './Modal.module.css';
+import { Routes, Route } from 'react-router-dom';
+import FormElem from '../FormElem/FormElem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Modal = ({ active, setActive }) => {
   return (
     <div className={`${s.modal} ${active && s.active}`}>
       <div className={`${s.modal_content} ${active && s.active}`}>
-        <button onClick={() => setActive(false)}>X</button>
+        <FontAwesomeIcon
+          onClick={() => setActive(false)}
+          className={s.x_mark}
+          icon={faXmark}
+        />
+        <Routes>
+          <Route
+            path='/reg'
+            element={
+              <FormElem
+                title={'Registration'}
+                link={'/login'}
+                input={{
+                  email: 'Email',
+                  password: 'Password',
+                  secondPassword: 'Enter password one more time',
+                }}
+                button={{ redirect: 'Login', submit: 'Register' }}
+                infoText={
+                  'By registering on the site, you agree to privacy and consent to the newsletter'
+                }
+                type={'reg'}
+              />
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <FormElem
+                title={'Login'}
+                link={'/reg'}
+                input={{
+                  email: 'Email',
+                  password: 'Password',
+                  secondPassword: 'Enter password one more time',
+                }}
+                button={{ redirect: 'Registration', submit: 'Login' }}
+                infoText={'Enter your account username and password'}
+                type={'login'}
+              />
+            }
+          />
+        </Routes>
       </div>
     </div>
   );
